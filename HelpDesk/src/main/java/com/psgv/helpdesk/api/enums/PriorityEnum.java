dfa5abcd04410383c79026933d136eef653aa80c
Gustavo <gustavo.s.torres@hotmail.com>
2018-04-05 22:23:45 -0300

    initial commit

---
+++ HelpDesk/src/main/java/com/psgv/helpdesk/api/enums/PriorityEnum.java
@@ -0,0 +1,7 @@
+package com.psgv.helpdesk.api.enums;
+
+public enum PriorityEnum {
+	HIGH,
+	NORMAL,
+	LOW
+}
---
+++ HelpDesk/src/main/java/com/psgv/helpdesk/api/enums/StatusEnum.java
@@ -0,0 +1,22 @@
+package com.psgv.helpdesk.api.enums;
+
+public enum StatusEnum {
+
+	NEW, 
+	ASSIGNED, 
+	RESOLVED, 
+	DISAPPROVED, 
+	CLOSED;
+
+	public static StatusEnum getStatus(String status) {
+		
+		StatusEnum[] values = values();
+		
+		for (StatusEnum statusEnum : values) {
+			if (statusEnum.name().equals(status)) {
+				return statusEnum;
+			}
+		}
+		return NEW;
+	}
+}
