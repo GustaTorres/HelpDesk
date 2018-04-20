c0d62e9c99b6c5d2d107e0eacf610a88ddeb46eb
Gustavo Torres <gustavo.s.torres@hotmail.com>
2018-04-20 16:42:38 -0300

    Add files via upload

--- HelpDesk/src/main/java/com/psgv/helpdesk/api/service/TicketService.java
+++ HelpDesk/src/main/java/com/psgv/helpdesk/api/service/TicketService.java
@@ -8,8 +8,10 @@ import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.psgv.helpdesk.api.dto.SummaryDTO;
 import com.psgv.helpdesk.api.entity.ChangeStatus;
 import com.psgv.helpdesk.api.entity.Ticket;
+import com.psgv.helpdesk.api.entity.User;
 import com.psgv.helpdesk.api.enums.StatusEnum;
 import com.psgv.helpdesk.api.repository.ChangeStatusRepository;
 import com.psgv.helpdesk.api.repository.TicketRepository;
@@ -19,10 +21,13 @@ public class TicketService extends CrudService<Ticket, String> {
 	
 	@Autowired
 	private ChangeStatusRepository changeStatusRepository;
+	
+	private TicketRepository ticketRepository;
 
 	@Autowired
 	public TicketService(TicketRepository repo) {
 		super(repo);
+		this.ticketRepository = repo;
 	}
 	
 	public Iterable<ChangeStatus> findAllChangeStatusByTicketId(String ticketId){
@@ -59,5 +64,60 @@ public class TicketService extends CrudService<Ticket, String> {
 		
 		ticket.setChanges(listChanges);
 		return ticket;
+	}
+
+	public void changeStatus(Ticket ticket, User userChange) {
+		ChangeStatus newStatus = new ChangeStatus();
+		newStatus.setUser(userChange);
+		newStatus.setDateChangeStatus(new GregorianCalendar());
+		newStatus.setStatus(ticket.getStatus());
+		newStatus.setTicket(ticket);
+		changeStatusRepository.save(newStatus);
+	}
+
+	public SummaryDTO findSummary() {
+		Integer amountNew = 0;
+		Integer amountResolved = 0;
+		Integer amountApproved = 0;
+		Integer amountDispproved = 0;
+		Integer amountAssigned = 0;
+		Integer amountClosed = 0;
+		
+		SummaryDTO dto = new SummaryDTO();
+		
+		List<Ticket> all = this.ticketRepository.findAll();
+		
+		for (Ticket ticket : all) {
+			
+			switch (ticket.getStatus()) {
+			case NEW:
+				amountNew++;
+				continue;
+			case RESOLVED:
+				amountResolved++;
+				continue;
+			case APPROVED:
+				amountApproved++;
+				continue;
+			case DISAPPROVED:
+				amountDispproved++;
+				continue;
+			case ASSIGNED:
+				amountAssigned++;
+				continue;
+			case CLOSED:
+				amountClosed++;
+				continue;
+			}
+		}
+		
+		dto.setAmountNew(amountNew);
+		dto.setAmountResolved(amountResolved);
+		dto.setAmountApproved(amountApproved);
+		dto.setAmountDispproved(amountDispproved);
+		dto.setAmountAssigned(amountAssigned);
+		dto.setAmountClosed(amountClosed);
+		
+		return dto;
 	}
 }
